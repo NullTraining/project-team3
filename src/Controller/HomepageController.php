15a9117eb485e505375ac0dba90fcf8bf3af5f1b
leovujanic <leonard.vujanic@gmail.com>
2018-01-11 00:25:34 +0100

    Task #13: Updated home action to find all available workshops

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use App\Repository\WorkshopRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class HomepageController
+ *
+ * @package App\Controller
+ */
 class HomepageController
 {
-
+    /**
+     * @var WorkshopRepository
+     */
+    private $repository;
+    
+    /**
+     * HomepageController constructor.
+     *
+     * @param WorkshopRepository $repository
+     */
+    public function __construct(WorkshopRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+    
+    /**
+     * @Template("workshop/home.list.html.twig")
+     * @return array
+     */
     public function indexAction()
     {
-        return new JsonResponse(['id'=>1,'name'=>'John']);
+        return ['workshops' => $this->repository->findAll(),];
     }
-
+    
     /**
      * @Route("/hello")
      */
