b35ba3a41801dc94dfacaef7296058ba3abf8519
leovujanic <leonard.vujanic@gmail.com>
2018-01-13 09:22:33 +0100

    Updated homepage test

--- tests/Controller/HomepageControllerTest.php
+++ tests/Controller/HomepageControllerTest.php
@@ -3,19 +3,18 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 
 class HomepageControllerTest extends WebTestCase
 {
-
-    public function testHomepage()
+    
+    public function testHomepageWhenWorkshopsDoNotExist()
     {
         $url = '/';
-
+        
         $client = self::createClient();
-
+        
         $client->request('GET', $url);
-
-        self::assertEquals('{"id":1,"name":"John"}', $client->getResponse()->getContent());
+        
+        self::assertContains('Currently, there are no active workshops', $client->getResponse()->getContent());
     }
 }
