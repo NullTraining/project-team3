00cf6b8e7bc220da7a731230af43b3146dcc1c92
leovujanic <leonard.vujanic@gmail.com>
2018-01-30 07:25:56 +0100

    #60 - Added workshop applicant invite controller

--- src/Controller/Admin/WorkshopApplicantController.php
+++ src/Controller/Admin/WorkshopApplicantController.php
@@ -12,6 +12,7 @@ use App\Entity\WorkshopApplicant;
 use App\Repository\UserRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 use FOS\UserBundle\Model\UserManager;
+use FOS\UserBundle\Util\TokenGenerator;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -25,17 +26,23 @@ class WorkshopApplicantController extends BaseAdminController
     /**
      * @var UserRepository
      */
-    public $userRepository;
+    private $userRepository;
     
+    /**
+     * WorkshopApplicantController constructor.
+     *
+     * @param UserRepository $userRepository
+     */
     public function __construct(UserRepository $userRepository)
     {
         $this->userRepository = $userRepository;
     }
     
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function approveAction()
     {
-        
-        $id = $this->request->query->get('id');
         $easyadmin = $this->request->attributes->get('easyadmin');
         /**
          * @var $applicantEntity WorkshopApplicant
@@ -45,21 +52,30 @@ class WorkshopApplicantController extends BaseAdminController
         $user = $this->userRepository->findByUsername($applicantEntity->getContactEmailAddress());
         
         if ($user !== null) {
-            throw new \Exception('User all ready in database.');
+            return $this->render('admin/workshop-applicant/confirm-approve.html.twig', [
+                'entity' => $applicantEntity,
+                'error'  => [
+                    'success' => false,
+                    'msg'     => 'Ooops! User all ready in database.',
+                ],
+            ]);
         }
         
         $form = $this->createFormBuilder()
             ->add('confirm_invite', HiddenType::class, [
-                'data' => 1,
+                'data'     => 1,
                 'required' => true,
             ])
             ->add('ok', SubmitType::class)
             ->getForm();
-    
-    
+        
+        
         $form->handleRequest($this->request);
         if ($form->isSubmitted() && $form->isValid()) {
-    
+            
+            $tokenGenerator = new TokenGenerator();
+            $token = $tokenGenerator->generateToken();
+            
             /**
              * @var $userManager UserManager
              */
@@ -68,16 +84,35 @@ class WorkshopApplicantController extends BaseAdminController
             $newUser->setUsername($applicantEntity->getContactEmailAddress());
             $newUser->setEmail($applicantEntity->getContactEmailAddress());
             $newUser->setEmailCanonical($applicantEntity->getContactEmailAddress());
-            $newUser->setEnabled(true);
-            $ti = $userManager->updateUser($newUser);
-            $ja = '';
-            die('submit');
+            $newUser->setEnabled(false);
+            $newUser->setPlainPassword($token);
+            $newUser->setConfirmationToken($token);
+            try {
+                $userManager->updateUser($newUser);
+                /**
+                 * Created 30/01/2018
+                 * TODO[leovujanic] - send email here
+                 */
+                $success = true;
+                $msg = 'User successfully invited.';
+            } catch (\Exception $e) {
+                $success = false;
+                $msg = 'Ooops! Something went wrong';
+            }
+            
+            return $this->render('admin/workshop-applicant/confirm-approve.html.twig', [
+                'entity' => $applicantEntity,
+                'error'  => [
+                    'success' => $success,
+                    'msg'     => $msg,
+                ],
+            ]);
         }
-        
         
         return $this->render('admin/workshop-applicant/confirm-approve.html.twig', [
             'entity' => $applicantEntity,
             'form'   => $form->createView(),
         ]);
     }
+    
 }
