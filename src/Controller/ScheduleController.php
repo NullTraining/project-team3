33280c337210817b86244e7b24ffe0c0ee0f4cb9
leovujanic <leonard.vujanic@gmail.com>
2018-01-31 18:56:31 +0100

    #112 - Added Controller

---
+++ src/Controller/ScheduleController.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+/**
+ * User: leonardvujanic
+ * DateTime: 31/01/2018 18:46
+ *
+ *
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ScheduleController
+ *
+ * @package App\Controller
+ */
+class ScheduleController
+{
+    
+    /**
+     * @Route("/schedule", name="schedule_index")
+     * @Template("schedule/index.html.twig")
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+}
