af42d10ace912e42d6ce1f596dbd2c561f871fcc
div3r <div3r.zg@gmail.com>
2018-02-02 17:34:54 +0100

    datetime probljem

---
+++ src/DataFixtures/ORM/WorkshopFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use App\Entity\Workshop;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+
+
+class WorkshopFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+        $html = new Workshop();
+        $html->setTitle('HTML basics');
+        $html->setDescription('In this workshop we will cover html basics');
+        $html->setDate(new DateTime());
+        $html->setActive(true);
+
+        $css = new Workshop();
+        $css->setTitle('CSS basics');
+        $css->setDescription('In this workshop we will cover css basics');
+        $html->setDate(new DateTime());
+        $css->setActive(false);
+
+
+        $manager->persist($html);
+        $manager->persist($css);
+        $manager->flush();
+
+        $this->addReference('workshop-html', $html);
+        $this->addReference('workshop-html', $css);
+
+
+    }
+
+    public function getOrder()
+    {
+        return 10;
+    }
+}
