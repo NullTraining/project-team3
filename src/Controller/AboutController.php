b41b3d5337f783c7dd82c5dc22959e9d7f64d678
div3r <div3r.zg@gmail.com>
2018-02-08 18:14:07 +0100

    added about page
    Closes: #128

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Class ContactController
+ *
+ * @package App\Controller
+ */
+class AboutController extends Controller
+{
+
+
+    /**
+     * @Route("/about", name="about")
+     * @Template("homepage/about.html.twig")
+     * @return array
+     */
+
+    public function aboutAction(Request $request)
+    {
+
+        return ;
+    }
+
+
+}
