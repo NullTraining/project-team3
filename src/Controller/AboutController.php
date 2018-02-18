8a6c476ac9d1131f657335df1212f52495b9ad4d
leovujanic <leonard.vujanic@gmail.com>
2018-02-18 14:59:24 +0100

    #141 - Update

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -4,23 +4,18 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class ContactController
- *
- * @package App\Controller
+ * Class ContactController.
  */
 class AboutController extends Controller
 {
-    
     /**
      * @Route("/about", name="about")
      * @Template("homepage/about.html.twig")
      */
     public function aboutAction()
     {
-    
     }
 }
