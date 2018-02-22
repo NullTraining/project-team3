42297afa7f74a25d1d2c911620b04b4d8d7c8159
leovujanic <leonard.vujanic@gmail.com>
2018-02-23 00:10:20 +0100

    #142 - phpcs fix

--- tests/Controller/HomepageControllerTest.php
+++ tests/Controller/HomepageControllerTest.php
@@ -5,27 +5,25 @@ namespace App\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
- * Class HomepageControllerTest
- *
- * @package App\Tests\Controller
+ * Class HomepageControllerTest.
  */
 class HomepageControllerTest extends WebTestCase
 {
     public function testHomepageWhenWorkshopsDoNotExist()
     {
-        $url = '/';
+        $url     = '/';
         $content = '';
-        
+
         $client = self::createClient();
-        
+
         $client->request('GET', $url);
-        
+
         $response = $client->getResponse();
-        
+
         if ($response) {
             $content = $response->getContent();
         }
-        
+
         self::assertContains('Null Training - Team 3', $content);
     }
 }
