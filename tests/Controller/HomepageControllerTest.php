1ab2b9aad24d29882bb05d25ae35c5d32e100002
leovujanic <leonard.vujanic@gmail.com>
2018-02-22 23:59:08 +0100

    #142 - updated tests - added check for null from getResponse()

--- tests/Controller/HomepageControllerTest.php
+++ tests/Controller/HomepageControllerTest.php
@@ -4,16 +4,28 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class HomepageControllerTest
+ *
+ * @package App\Tests\Controller
+ */
 class HomepageControllerTest extends WebTestCase
 {
     public function testHomepageWhenWorkshopsDoNotExist()
     {
         $url = '/';
-
+        $content = '';
+        
         $client = self::createClient();
-
+        
         $client->request('GET', $url);
-
-        self::assertContains('Null Training - Team 3', $client->getResponse()->getContent());
+        
+        $response = $client->getResponse();
+        
+        if ($response) {
+            $content = $response->getContent();
+        }
+        
+        self::assertContains('Null Training - Team 3', $content);
     }
 }
