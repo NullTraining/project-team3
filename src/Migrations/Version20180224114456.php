2714fc853c4e729f4968a57bb1bdb41ac5547978
leovujanic <leonard.vujanic@gmail.com>
2018-02-24 12:47:09 +0100

    #157 - Deleted old invalid migration and added new.

--- src/Migrations/Version20180224114456.php
+++ src/Migrations/Version20180224114456.php
@@ -10,14 +10,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180224093743 extends AbstractMigration
+class Version20180224114456 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE users CHANGE team_id team_id INT DEFAULT NULL, CHANGE salt salt VARCHAR(255) DEFAULT NULL, CHANGE last_login last_login DATETIME DEFAULT NULL, CHANGE confirmation_token confirmation_token VARCHAR(180) DEFAULT NULL, CHANGE password_requested_at password_requested_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE users ADD is_email_visible TINYINT(1) DEFAULT \'0\' NOT NULL');
     }
 
     public function down(Schema $schema)
@@ -25,6 +25,6 @@ class Version20180224093743 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE users CHANGE team_id team_id INT DEFAULT NULL, CHANGE salt salt VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8_unicode_ci, CHANGE last_login last_login DATETIME DEFAULT \'NULL\', CHANGE confirmation_token confirmation_token VARCHAR(180) DEFAULT \'NULL\' COLLATE utf8_unicode_ci, CHANGE password_requested_at password_requested_at DATETIME DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE users DROP is_email_visible');
     }
 }
